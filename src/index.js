cb2e8237a02c80ff57d1a64891000092b0a7e362
Luciano Ayres <lucianofarias@gmail.com>
2022-06-26 10:13:14 -0300

    feat: finish chapter 04

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {loadDevTools} from './dev-tools/load'
 import './bootstrap'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import {App} from './app'
+import {App} from './app.exercise'
 
 loadDevTools(() => {
   ReactDOM.render(<App />, document.getElementById('root'))
