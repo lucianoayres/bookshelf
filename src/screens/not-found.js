543a54ad0c71faf0a795fcc452fec9eca7018a47
Luciano Ayres <lucianofarias@gmail.com>
2022-06-26 19:29:09 -0300

    feat: finish chapter 05

--- src/screens/not-found.js
+++ src/screens/not-found.js
@@ -1,3 +1,26 @@
+// export * from './not-found.exercise'
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
+
+import {Link} from 'components/lib'
+
 export * from './not-found.final'
 
-// export * from './not-found.exercise'
+function NotFoundScreen() {
+  return (
+    <div
+      css={{
+        height: '100%',
+        display: 'grid',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div>
+        Sorry... nothing here. <Link to="/discover">Go home</Link>
+      </div>
+    </div>
+  )
+}
+
+export {NotFoundScreen}
