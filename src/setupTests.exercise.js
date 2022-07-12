a4e5b46577abac4bfd70529693e5cb9e554550e1
Luciano Ayres <lucianofarias@gmail.com>
2022-07-11 22:00:01 -0300

    feat: finish 13-integration-testing

--- src/setupTests.exercise.js
+++ src/setupTests.exercise.js
@@ -6,11 +6,14 @@ import * as booksDB from 'test/data/books'
 import * as listItemsDB from 'test/data/list-items'
 import * as usersDB from 'test/data/users'
 
+jest.mock('components/profiler')
 // enable API mocking in test runs using the same request handlers
 // as for the client-side mocking.
 beforeAll(() => server.listen())
 afterAll(() => server.close())
 afterEach(() => server.resetHandlers())
+
+beforeEach(() => jest.useRealTimers())
 
 afterEach(async () => {
   queryCache.clear()
